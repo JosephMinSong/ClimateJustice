cfde72d6f51153a3927eb7527a484e6e66785e79
Joseph Song <song.josephmin@gmail.com>
2024-10-28 17:36:43 -0700

    first commit

---
+++ flask_backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import requests
+import os
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+
+@app.route('/api/seattle_weather')
+def weather():
+    api_key = os.environ.get('WEATHER_API')
+    parameters = {'key': api_key, 'place_id': 'london'}
+    url = "https://www.meteosource.com/api/v1/free/point"
+    response = requests.get(url, params=parameters)
+
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify({'message': f"Weather data: {data}"})
+    else:
+        return jsonify({'message': 'Failed to fetch weather data'}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
