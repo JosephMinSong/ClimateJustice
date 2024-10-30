136ba65cf6e052a06dff90f4b543474aeafe92a3
Joseph Song <song.josephmin@gmail.com>
2024-10-29 20:04:03 -0700

    from laptop

--- api/app.py
+++ api/app.py
@@ -1,20 +1,20 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
 import requests
 import os
 
 app = Flask(__name__)
 
+
 @app.route('/api/seattle_weather')
 def weather():
-    api_key = os.environ.get('WEATHER_API')
-    parameters = {'key': api_key, 'place_id': 'london'}
-    url = "https://www.meteosource.com/api/v1/free/point"
-    response = requests.get(url, params=parameters)
+    api_key = os.environ.get('WEATHER_API_KEY')
+    # parameters = {'key': api_key, 'place_id': 'seattle'}
+    url = f"https://www.meteosource.com/api/v1/flexi/time_machine?place_id=london&date=2021-03-15&timezone=UTC&units=auto&key={api_key}"
+    response = requests.get(url)
 
     if response.status_code == 200:
-        data = response.json()
+        res = response.json()
+        data = res['data']
         return jsonify({'message': f"Weather data: {data}"})
     else:
         return jsonify({'message': 'Failed to fetch weather data'}), 500
-
